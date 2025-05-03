a49cb34c9a645a9dec05ef5ae2858ff844d9457a
Norbert Baliga <baliga.norbert@gmail.com>
2025-05-03 21:22:43 +0200

    Fix issues with the API

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,28 +1,32 @@
-const fetch = require('node-fetch').default;
-
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
-    'admin': 'd8a60c1d-1181-4954-a269-2740704caeca'
+    'admin': 'd8a60c1d-1181-4954-a269-2740704caeca',
+    'editor': '9406617a-e9bd-4083-9756-875054358e7e'
 };
 
 module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = [];
     
+    context.log(`GetRoles function received request: ${JSON.stringify(req)}`);
+
     for (const [role, groupId] of Object.entries(roleGroupMappings)) {
         if (await isUserInGroup(groupId, user.accessToken)) {
             roles.push(role);
         }
     }
 
+    context.log(`User roles: ${roles.join(', ')}`);
+
     context.res.json({
-        roles
+        "roles": roles
     });
 }
 
 async function isUserInGroup(groupId, bearerToken) {
     const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
     url.searchParams.append('$filter', `id eq '${groupId}'`);
+    
     const response = await fetch(url, {
         method: 'GET',
         headers: {
@@ -36,5 +40,6 @@ async function isUserInGroup(groupId, bearerToken) {
 
     const graphResponse = await response.json();
     const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
+    
     return matchingGroups.length > 0;
 }
